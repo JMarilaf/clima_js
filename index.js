3e43dae0e2cfe52a63d6622f1101b9069aee4ad2
JMarilaf <javiermarilaf@gmail.com>
2021-10-17 19:52:42 -0300

    JMarilaf
    V0.9

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
+const API_KEY = '98a5403819a80eae71a1394e1d0e4fed';
 
-const API_KEY = '98a5403819a80eae71a1394e1d0e4fed'
+let iconoClima = document.getElementById('image-icon');
 
 const fetchData = position => {
 
     const { latitude, longitude } = position.coords;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
         .then(response => response.json())
         .then(data => setWeatherData(data));
-
 }
 
 const setWeatherData = data => {
     console.log(data);
     const weatherData = {
-        location: data.name,
-        description: data.weather[0].description,
-        humidity: data.main.humidity,
-        pressure: data.main.pressure,
-        temperature: data.main.temperature,
-        date: getDate(),
+        description: `Tiempo: ${data.weather[0].description}`,
+        tempMax: `Temperatura máxima: ${data.main.temp_max} °C`,
+        location: `Ciudad: ${data.name}`,
+        //Icono
+        tempMin: `Temperatura mínima: ${data.main.temp_min} °C`,
+        temperature: `Temperatura actual: ${data.main.temp} °C`,
+        pressure: `Presión: ${data.main.temp} mb`,
+        wind: `Velocidad del viento: ${data.wind.speed} m/s`,
+        humidity: `Humedad: ${data.main.humidity}%`,
+        date: `Fecha: ${getDate()}`,
     }
 
     Object.keys(weatherData).forEach( key => {
         document.getElementById(key).textContent = weatherData[key];
     });
 
+    //Mostrar icono
+    let iconCode = data.weather[0].icon;
+    const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`;
+    iconoClima.src = urlIcon;
 }
 
 const getDate = () => {
@@ -33,6 +41,7 @@ const getDate = () => {
     return `${date.getDate()}-${ ( '0' + (date.getMonth() + 1)).slice(-2)  }-${date.getFullYear()}`;
 }
 
-const  onLoad = () => {
+const onLoad = () => {
     navigator.geolocation.getCurrentPosition(fetchData);
 }
+
