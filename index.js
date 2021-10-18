0620769710a1b58dca1ac6f6e472f672241fb890
JMarilaf <javiermarilaf@gmail.com>
2021-10-18 19:28:11 -0300

    +Fix

--- index.js
+++ index.js
@@ -14,26 +14,62 @@ const fetchData = position => {
 const setWeatherData = data => {
     console.log(data);
     const weatherData = {
-        description: `Tiempo: ${data.weather[0].description}`,
-        tempMax: `Temperatura máxima: ${data.main.temp_max} °C`,
-        location: `Ciudad: ${data.name}`,
-        //Icono
-        tempMin: `Temperatura mínima: ${data.main.temp_min} °C`,
-        temperature: `Temperatura actual: ${data.main.temp} °C`,
-        pressure: `Presión: ${data.main.temp} mb`,
-        wind: `Velocidad del viento: ${data.wind.speed} m/s`,
-        humidity: `Humedad: ${data.main.humidity}%`,
-        date: `Fecha: ${getDate()}`,
+        description: `${data.weather[0].description}`,
+        tempMax: `${data.main.temp_max} °C`,
+        location: `${data.name}`,
+        tempMin: `${data.main.temp_min} °C`,
+        temperature: `${data.main.temp} °C`,
+        pressure: `${data.main.temp} mb`,
+        wind: `${data.wind.speed} m/s`,
+        humidity: `${data.main.humidity}%`,
+        date: `${getDate()}`,
     }
 
     Object.keys(weatherData).forEach( key => {
         document.getElementById(key).textContent = weatherData[key];
     });
 
-    //Mostrar icono
     let iconCode = data.weather[0].icon;
-    const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`;
-    iconoClima.src = urlIcon;
+
+    //Mostrar iconos animados en carpeta local
+    switch (iconCode) {
+        case '01d':
+            iconoClima.src = 'animated/clear-sky-day.svg'
+        break;
+        case '01n':
+            iconoClima.src = 'animated/clear-sky-night.svg'
+        break;
+        case '02d':
+            iconoClima.src = 'animated/few-clouds-day.svg'
+        break;
+        case '02n':
+            iconoClima.src = 'animates/few-clouds-day.svg'
+        break;
+        case '03d':
+        case '03n':
+            iconoClima.src = 'animated/scattered-clouds'
+        break;
+        case '04d':
+        case '04n':
+            iconoClima.src = 'animated/broken-clouds.svg'
+        break;
+        case '09d':
+        case '09n':
+        case '10d':
+        case '10n':
+            iconoClima.src = 'animated/rain.svg'
+        break;
+        case '11d':
+        case '11n':
+            iconoClima.src = 'animated/thunderston.svg'
+        break;
+        default:
+            iconoClima.src = 'animated/mist.png'
+    }
+
+    //Mostrar icono utilizando api de "openweather"
+    // const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`;
+    // iconoClima.src = urlIcon;
 }
 
 const getDate = () => {
@@ -44,4 +80,3 @@ const getDate = () => {
 const onLoad = () => {
     navigator.geolocation.getCurrentPosition(fetchData);
 }
-
